506c34d6a98dffb0cc42e86440dacad2d80995af
wilberthRivasB <wilberth.rivas@ensitech.com>
2019-02-20 12:00:19 -0600

    Cambio en el http de css y js por https

--- resources/views/shared/main_layout.blade.php
+++ resources/views/shared/main_layout.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{asset('css/libraries.css')}}">  
+        <link rel="stylesheet" href="{{secure_asset('css/libraries.css')}}">  
         <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" >-->
         @yield('styles') 
         
@@ -14,7 +14,7 @@
             @yield('content')  
         </div>
 
-        <script src="{{asset('js/libraries.js')}}" type="text/javascript"></script>
+        <script src="{{secure_asset('js/libraries.js')}}" type="text/javascript"></script>
         @yield('scripts')
     </body>
 </html>
